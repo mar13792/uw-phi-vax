e163c885dcd6c0f4ebcb0efa0c4f6138a4350b79
Frank31 <frc2@uw.edu>
2021-09-02 15:04:42 -0600

    Renamed and re-ordered files

--- 6b_mov_analyses.R
+++ 6b_mov_analyses.R
@@ -64,11 +64,156 @@ pvalue <- function(x, ...) {
   c("", sub("<", "&lt;", format.pval(p, digits=3, eps=0.001)))
 }
 
+###################
+# mother's education
+###################
+
+dt$edu <- as.character(dt$v106)
+dt <- dt %>% mutate(edu = recode(edu,
+                                 `0`=0,
+                                 `1`=1,
+                                 `2`= 2,
+                                 `3`=2))
+dt$edu <- factor(dt$edu,
+                 levels = c(0,1,2),
+                 labels = c("no education", "primary", "secondary or higher"))
+
+###################
+# mother's literacy levels
+###################
+dt$literate <- as.character(dt$v155)
+
+dt <- dt %>% replace_with_na(replace = list(literate = 9))
+
+dt <- dt %>% mutate(literate = recode(literate,
+                                      `0`=0,
+                                      `1`=1,
+                                      `2`=1,
+                                      `3`=9,
+                                      `4`=9))
+
+dt$literate <- factor(dt$literate,
+                      levels = c(0,1),
+                      labels = c("iliterate", "literate"))
+
+###################
+# mother's age category
+###################
+
+dt$wom_agecat <- dt$v012
+
+dt <- dt %>%
+  mutate(wom_agecat=case_when(
+    wom_agecat %in% 15:19 ~ "1",
+    wom_agecat %in% 20:34 ~ "2",
+    wom_agecat %in% 35:49 ~ "3"))
+
+dt$wom_agecat <- factor(dt$wom_agecat,
+                        levels = c(1,2,3),
+                        labels = c("15-19", "20-34", "35-49"))
+
+###################
+# parity
+###################
+dt$total_children_born <- dt$v201 
+
+dt <- dt %>% 
+  mutate(total_children_born=case_when(
+    total_children_born %in% 1 ~ "1",
+    total_children_born %in% 2:3 ~ "2",
+    total_children_born %in% 4:5 ~ "3",
+    total_children_born %in% 6:20 ~ "4"
+  ))
+
+dt$total_children_born <- factor(dt$total_children_born,
+                                 levels = c(1,2,3,4),
+                                 labels = c("1 child", "2-3 children", "4-5 children", "6+ children"))
+
+###################
+# marital status
+###################
+dt$marital <- as.character(dt$v501)
+
+dt <- dt %>% mutate(marital = recode(marital,
+                                     `0`=1,
+                                     `1`=2,
+                                     `2`=3,
+                                     `3`=4,
+                                     `4`=4,
+                                     `5`=4))
+
+dt$marital<- factor(dt$marital,
+                    levels = c(1,2,3,4),
+                    labels = c("single", "married", "union", "divorced, seperated, widowed, or other"))
+
+###################
+# mother's employment
+###################
+dt$wom_occ <- dt$v717
+
+dt <- dt %>% replace_with_na(replace = list(wom_occ = 99))
+
+dt <- dt %>% 
+  mutate(wom_occ=case_when(
+    wom_occ %in% 0 ~ "1",
+    wom_occ %in% 1:97 ~ "2"
+  ))
+
+dt$wom_occ <- factor(dt$wom_occ,
+                     levels = c(1,2),
+                     labels = c("not employed in last 12 months", "employed"))
+
+###################
+# assets
+###################
+
+# each survey should have either v190a or v190 for the household assets
+dt$assets <- ifelse(!is.na(dt$v190a), dt$v190a, dt$v190)
+
+dt$assets <- as.factor(dt$assets)
+
+###################
+# average household size
+###################
+dt$hhsize <- dt$v136
+
+###################
+# urban
+###################
+dt$urban <- abs(dt$v025-2)
+
+dt$urban <-factor(dt$urban,
+                  levels = c(0,1),
+                  labels = c("rural household", "urban household"))
+
+###################
+# sex of head of household
+###################
+dt$female_head <- dt$v151
+
+dt$female_head <- factor(dt$female_head,
+                         levels = c(1,2),
+                         labels = c('male', 'female'))
+
+###################
+# asign labels to variable names
+###################
+dt$sex_of_child <- factor(dt$sex_of_child,
+                          levels=c(1,2),
+                          labels=c("male", "female"))
+
+###################
+#kid age category
+###################
+
+dt$kid_agecat <- round(time_length(difftime(dt$intv_date, dt$dob), "years"), digits = 0)
+dt$kid_agecat <- as.factor(dt$kid_agecat)
+
 # format data structures
 data$mea1_missed_opportunity <-factor(data$mea1_missed_opportunity,
                                        levels=c(0,1),
                                        labels=c("No", "Yes"))
-label(data$female) <- "Child's gender"
+label(data$sex_of_child) <- "Child's sex"
 label(data$edu) <- "Mother's education"
 label(data$wom_occ) <- "Mother's occupation"
 label(data$hhsize) <- "Household size"
@@ -76,9 +221,10 @@ label(data$female_head) <-"Sex of head of household"
 label(data$urban) <-"Household location"
 label(data$mea1_missed_opportunity) <-"Missed measles opportunity"
 
-table1(~  female + edu + wom_occ + hhsize + urban + female_head| mea1_missed_opportunity, data=data[v000=="NG7"], overall=F, extra.col=list(`P-value`=pvalue))
+data1 <- data %>% filter(v000=="NG7")
+table1(~  sex_of_child + edu + wom_occ + hhsize + urban + female_head| mea1_missed_opportunity, data=data1, overall=F, extra.col=list(`P-value`=pvalue))
 
-table1(~  female + edu + wom_agecat +  + wom_occ + assets + hhsize + urban + female_head | mea1_missed_opportunity, data=data, overall=F, extra.col=list(`P-value`=pvalue))
+table1(~  sex_of_child + edu + wom_agecat +  + wom_occ + assets + hhsize + urban + female_head | mea1_missed_opportunity, data=data, overall=F, extra.col=list(`P-value`=pvalue))
 
 
 # create a series of tables   
